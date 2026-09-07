4e756d85f8508ff0aceab19499d7b83cd215fcd9
sunwen <1102633262@qq.com>
2026-09-07 17:15:58 +0800

    build: select Win32 solution for x32

--- vcbuild.bat
+++ vcbuild.bat
@@ -452,6 +452,7 @@ if defined nobuild goto :after-build
 set "msbcpu=/m:2"
 if "%NUMBER_OF_PROCESSORS%"=="1" set "msbcpu=/m:1"
 set "msbplatform=x64"
+if "%target_arch%"=="x86" set "msbplatform=Win32"
 if "%target_arch%"=="arm64" set "msbplatform=ARM64"
 if "%target%"=="Build" (
   if defined no_cctest set target=node
